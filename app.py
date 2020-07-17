8d0e3d07efbfed442ee603543a629d813f6edaef
lijah-tech-solutions <lekswyde205@gmail.com>
2020-07-17 16:10:54 +0100

    Registration form added / Refactor

--- app.py
+++ app.py
@@ -1,30 +1,64 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, flash, url_for, session, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, FormField, TextAreaField, PasswordField, validators, StringField
+from passlib.hash import sha256_crypt
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-db = SQLAlchemy(app)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-class BlogPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(20), nullable=False, default='N/A')
-    date_posted = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
-
-    def __repr__(self):
-        return 'Blog post' + str(self.id)
+# Initialize MYSQL
+mysql = MySQL(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/home/<int:id>')
-def hello(id):
-    return str(id) + " No 1"
+class RegisterForm(Form):
+    name = StringField(u'Name', validators=[validators.input_required(), validators.Length(min=1, max=50)])
+    username  = StringField(u'Username', validators=[validators.input_required(), validators.Length(min=1, max=50)])
+    email  = StringField(u'Email', validators=[validators.input_required(), validators.Length(min=6, max=50)])
+    password  = PasswordField(u'Password', validators=[
+        validators.Length(min=6, max=50),
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+        ])
+    confirm  = PasswordField(u'Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        
+        #Creating and executing Cursor
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password) )
+
+        #Commiting the Database 
+        mysql.connection.commit()
+
+        #Closing the Cursor
+        cur.close()
+
+        flash('You are now registered, kindly login', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
+# @app.route('/home/<int:id>')
+# def hello(id):
+#     return str(id) + " No 1"
 
 @app.route('/posts', methods=['GET', 'POST'])
 def posts():
@@ -77,10 +111,10 @@ def new_post():
         return render_template('new_post.html')
 
 
-
-@app.route('/only', methods=['POST'])
-def get_req():
-    return 'Authorized 2'
+# @app.route('/only', methods=['POST'])
+# def get_req():
+#     return 'Authorized 2'
 
 if __name__ == "__main__":
+    app.secret_key='secure456'
     app.run(debug=True)
